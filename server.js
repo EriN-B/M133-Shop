d42cf73306195734fa1a70c89ce2c7868ac803ee
Joshua Odermatt <joshi.odermatt@gmail.com>
2021-12-15 13:21:13 +0100

    working

--- server.js
+++ server.js
@@ -2,19 +2,51 @@
 import {Application} from "https://deno.land/x/oak@v6.3.1/mod.ts";
 import { apiRoutes } from "./backend/app.js";
 import { frontendFiles } from "./backend/fileserver.js";
-import { exec } from "https://deno.land/x/exec/mod.ts";
+
+
+let commands = ["echo moin", "echo hallo"];
 
 const app = new Application();
 
+commands.forEach((command) => {
+    const p = Deno.run(
+        {
+            cmd: [command.split(' ')[0], command.split(' ')[1]]
+        }
+    );
 
-await exec('cd ./frontend && npm i && ng build && cd ..');
+}.then(() => {
+    app.use(apiRoutes);
+    app.use(frontendFiles);
+    
+    
+    
+    app.listen({port: 8000});
 
-app.use(apiRoutes);
-app.use(frontendFiles);
+    console.log("running")
+}));
 
 
 
-app.listen({port: 8000});
 
 
-console.log("running")
+
+
+
+
+
+
+
+/*
+async function runCommands(commands) {
+    commands.forEach((command) => {
+        const p = Deno.run(
+            {
+                cmd: [command.split(' ')[0], command.split(' ')[1]]
+            }
+        );
+    
+        await p.status();
+    })
+}
+*/

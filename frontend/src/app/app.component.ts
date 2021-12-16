49f7da3c094acfec26c0894233eadae3dcac6191
Erin Bachmann <57983942+EriN-B@users.noreply.github.com>
2021-12-16 22:22:01 +0100

    added Toaster on Item-add

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { faShoppingCart, faWarehouse, faCartPlus } from '@fortawesome/free-solid-svg-icons';
-
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +8,14 @@ import { faShoppingCart, faWarehouse, faCartPlus } from '@fortawesome/free-solid
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private toastr: ToastrService) {}
   //Font-Awesome icons
   faShoppingCart = faShoppingCart;
   faWarehouse = faWarehouse;
   faCartPlus = faCartPlus;
+
+  showSuccess() {
+    this.toastr.success('Product added to your Cart');
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { ToastrModule } from 'ngx-toastr';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +17,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

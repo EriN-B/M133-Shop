04a4e64bfe9e0b545a220a4df4fa60654fc343f6
Erin Bachmann <57983942+EriN-B@users.noreply.github.com>
2021-12-16 21:26:00 +0100

    added home-screen

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faShoppingCart, faWarehouse, faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  //Font-Awesome icons
+  faShoppingCart = faShoppingCart;
+  faWarehouse = faWarehouse;
+  faCartPlus = faCartPlus;
 }

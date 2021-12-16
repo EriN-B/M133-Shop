687047ab8a0b0ec29b222b99946f23464dacebe0
Erin Bachmann <57983942+EriN-B@users.noreply.github.com>
2021-12-16 22:14:05 +0100

    added discount-badge

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { faShoppingCart, faWarehouse, faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',

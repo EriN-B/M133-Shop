01638208e8ab400c3b4fa40b851d90c10e12ed16
Joshua Odermatt <joshi.odermatt@gmail.com>
2022-01-12 09:04:35 +0100

    update api

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,26 @@
 'use strict'
 import { Router } from 'https://deno.land/x/oak@v6.3.1/mod.ts';
-import { v4 } from 'https://deno.land/std@0.77.0/uuid/mod.ts';
 import { Inventory } from './data/inventory.js';
+import { Session } from "https://deno.land/x/sessions/mod.ts";
+import {log} from './log.js'
 
 const router = new Router();
 
 let inventory = new Inventory();
 
-let cart = [];
+const logger = new log();
 
 router
-    .get("/api/products", context => context.response.body = inventory.products)
-    .get("/api/products/:title", context => context.response.body = inventory.products.find(item => item.title))
-    .post("/api/cart/", async context => {
+    .get("/api/products", context => {
+        logger.info("Fetching all products");
+        context.response.body = inventory.products})
+    .get("/api/products/:title", context => {
+        console.log(params.title)
+        logger.info("Fetching products by title");
+        context.response.body = inventory.products.find(item => item.title)})
+    .post("/api/cart", session.initMiddleware(), async context => {
+        await sessionStorage.setItem("")
+        logger.info("Adding Product to cart");
         const product = await context.request.body({ type: "json" }).value;
         console.log(product);
     });

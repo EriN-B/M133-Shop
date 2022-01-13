ef70f037bd121c705944bfbe1863cba2e00f7ad5
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2022-01-13 22:36:09 +0100

    update product-site

--- frontend/src/app/modal/modal.component.ts
+++ frontend/src/app/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService : ProductService) { }
+
+  product: any;
 
   ngOnInit(): void {
+    this.product = this.productService.getProduct();
   }
 
 }
--- frontend/src/app/products/products.component.ts
+++ frontend/src/app/products/products.component.ts
@@ -4,6 +4,7 @@ import { ToastrService } from 'ngx-toastr';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from '../modal/modal.component';
+import { ProductService } from '../services/product.service';
 
 
 @Component({
@@ -12,10 +13,10 @@ import { ModalComponent } from '../modal/modal.component';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  
+
   products: any = [];
 
-  constructor(private toastr: ToastrService, private http: HttpClient, private matDialog: MatDialog) {
+  constructor(private toastr: ToastrService, private http: HttpClient, private matDialog: MatDialog, private productService: ProductService) {
   }
 
   async ngOnInit() {
@@ -34,7 +35,8 @@ export class ProductsComponent implements OnInit {
     this.toastr.success(title + ' \n wurde zu deinem Account hinzugefügt');
   }
 
-  openDialog() {
+  openDialog(product:any) {
+    this.productService.setProduct(product)
     this.matDialog.open(ModalComponent);
   }
 }
